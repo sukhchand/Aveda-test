d0f965eb110c4ac4ca97ab7ba2277ae14c5da389
sukhchand <sukhchand.k@echidnainc.com>
2015-06-11 15:41:18 +0530

    my orders page

--- app/js/controllers/myordersViewCtrl.js
+++ app/js/controllers/myordersViewCtrl.js
@@ -13,7 +13,7 @@ four51.app.controller('myordersCtrl', ['$scope', '$rootScope', function($scope, 
   		orderType:'Standard',
   		orderPlaced:'06/12/2015',
   		total:'$56.97',
-  		status:'shipped'
+  		status:'cancelled'
   	},
   	{  	
   		orderId:'12485345-456',
@@ -29,7 +29,7 @@ four51.app.controller('myordersCtrl', ['$scope', '$rootScope', function($scope, 
   		orderType:'Standard',
   		orderPlaced:'06/12/2015',
   		total:'$56.97',
-  		status:'shipped'
+  		status:'pending'
   	},
   	{  	
   		orderId:'12485345-456',
@@ -37,7 +37,7 @@ four51.app.controller('myordersCtrl', ['$scope', '$rootScope', function($scope, 
   		orderType:'Standard',
   		orderPlaced:'06/12/2015',
   		total:'$56.97',
-  		status:'shipped'
+  		status:'pending'
   	},
   	{  	
   		orderId:'12485345-456',
@@ -45,7 +45,7 @@ four51.app.controller('myordersCtrl', ['$scope', '$rootScope', function($scope, 
   		orderType:'Standard',
   		orderPlaced:'06/12/2015',
   		total:'$56.97',
-  		status:'shipped'
+  		status:'cancelled'
   	},
   	{  	
   		orderId:'12485345-456',
@@ -53,20 +53,126 @@ four51.app.controller('myordersCtrl', ['$scope', '$rootScope', function($scope, 
   		orderType:'Standard',
   		orderPlaced:'06/12/2015',
   		total:'$56.97',
-  		status:'shipped'
-  	}
+  		status:'pending'
+  	},
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'pending'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'delivered'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'delivered'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'cancelled'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'delivered'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'delivered'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'pending'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'pending'
+    },
+    {   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'pending'
+    },{   
+      orderId:'12485345-456',
+      createdBy:'Ryan Belinger',
+      orderType:'Standard',
+      orderPlaced:'06/12/2015',
+      total:'$56.97',
+      status:'pending'
+    }
+
   ];
+  // angular.copy( $scope.Orders,$scope.OrdersCopy); 
+   $scope.OrdersCopy = $scope.Orders;
   $scope.myColDefs = [
-      {field: 'orderId', displayName: 'ORDER NUMBER'},
+      {field: 'orderId', displayName: 'ORDER NUMBER', cellTemplate: "<a href='#''>{{COL_FIELD}}</a> "},
       {field: 'createdBy', displayName: 'CREATED BY'},
       {field: 'orderType', displayName: 'ORDER TYPE'},
       {field: 'orderPlaced', displayName: 'ORDER PLACED'},
       {field: 'total', displayName: 'TOTAL'},
-      {field: 'status',displayName: 'STATUS', cellTemplate: '<button class="btn btn-primary" type="button" value="shyam"></button> '},
+      {field: 'status',displayName: 'STATUS', cellTemplate: "<button class='btn btn-primary status_icon' type='button' ng-class = 'COL_FIELD'><span></span>{{COL_FIELD}}</button> "},
       {field: '', displayName: '',cellTemplate: '<a href="">CREATE ORDER CLAIM</a>'}
 
 
   ];
   $scope.gridOptions = { columnDefs:$scope.myColDefs, data: 'Orders' };
-  
+
+  $scope.orderCategory=[
+    {
+      'type':'ORDER HISTORY',
+    },
+    {
+      'type':'PENDING ORDERS',
+    },
+    {
+      'type':'USER ORDERS',
+    }
+  ];
+  $scope.selected = 0;
+
+    $scope.select= function(index) {
+       $scope.selected = index;
+       if(index=='1'){   
+          $scope.Orders = _.pluck(_.where($scope.Orders, { 'status': 'pending'}));   
+       }
+       else{       
+        $scope.Orders = $scope.OrdersCopy;
+       }
+
+    };
+
 }]);
